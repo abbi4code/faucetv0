facc3929d9488bf2880f6e0dd28769f09908dd02
abhishek <abhishek4code@gmail.com>
2024-10-16 01:28:45 +0530

    feat: ui updated && destruct the connection fnc

--- components/Airdrop.tsx
+++ components/Airdrop.tsx
@@ -1,41 +1,52 @@
-"use client"
+"use client";
 
-import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-import React, { useState } from 'react'
-import { Label } from './ui/label'
-import { Input } from './ui/input'
-import { Button } from './ui/button'
-import { toast } from 'sonner'
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import React, { useState } from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { toast } from "sonner";
 
 export default function Airdrop() {
-    const connection = useConnection()
-    // console.log(connection.connection)
-    const {publicKey} = useWallet()
-    console.log(publicKey?.toString())
-    const [amount,setAmount] = useState(0);
-    const [address,setAddress] =useState("")
+  const connection = useConnection();
+  // console.log(connection.connection)
+  const { publicKey } = useWallet();
+//   console.log(publicKey?.toString());
+  const [amount, setAmount] = useState(0);
+  const [address, setAddress] = useState("");
 
-    const handleAirdrop = async()=>{
-        console.log("amount",amount)
-        const result = await connection.connection.requestAirdrop(publicKey,amount);
-        // console.log(result)
-        toast.success(`${amount} is credited to address ${publicKey?.toString()}`)
-        alert(`airdropped ${amount} to ${publicKey} and the result is ${result}`)
-        // alert(publicKey?.toBase58())
-    }
-
+  const handleAirdrop = async () => {
+    console.log("amount", amount);
+    const result = await connection.connection.requestAirdrop(
+      publicKey,
+      amount
+    );
+    // console.log(result)
+    toast.success(`${amount} is credited to address ${publicKey?.toString()}`);
+    alert(`airdropped ${amount} to ${publicKey} and the result is ${result}`);
+    // alert(publicKey?.toBase58())
+  };
 
   return (
-    <div className='flex flex-col'>
-        <Label>Enter Amount</Label>
-        <Input placeholder='Enter Amount' value={amount} onChange={(e)=> setAmount(Number(e.target.value))}/>
-        <Label>Enter Wallet Address</Label>
-        <Input placeholder='Enter Address' />
-        <Button disabled={amount === 0} className='w-full my-3 rounded-[4px]'  variant={'outline'} onClick={handleAirdrop}>Confirm Airdrop</Button>
-        
-
-        
-      
+    <div className="flex flex-col border border-gray-500 rounded-[4px] px-4 py-5 w-max md:min-w-[30rem] bg-red-300 z-10 ">
+      <Label className="font-semibold text-3xl tracking-wider">Enter Amount</Label>
+   
+      <Input
+        placeholder="Enter Amount"
+        className="rounded-[4px] border border-[#27272A]"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <Label className="font-semibold text-3xl tracking-wider">Enter Wallet Address</Label>
+      <Input placeholder="Enter Address" className="rounded-[4px] border border-[#27272A]"/>
+      <Button
+        disabled={amount === 0}
+        className="w-full my-3 rounded-[4px]"
+        variant={"outline"}
+        onClick={handleAirdrop}
+      >
+        Confirm Airdrop
+      </Button>
     </div>
-  )
+  );
 }
--- components/Connection.tsx
+++ components/Connection.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { FC, useMemo } from "react";
+import React, { FC, useEffect, useMemo } from "react";
 import {
   ConnectionProvider,
+  useWallet,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
@@ -12,34 +13,76 @@ import {
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
+import bgimg from "../public/bg.jpeg";
 
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
 import Airdrop from "./Airdrop";
+import { Label } from "./ui/label";
+import Image from "next/image";
 // import Airdrop from './Airdrop';
 
 export default function Connection() {
   // const network =WalletAdapterNetwork.Devnet
-  const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL || ""
-  console.log("rpcUrl",rpcUrl)
+  const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL || "";
+  console.log("rpcUrl", rpcUrl);
+
   // const endpoint = useMemo(()=>clusterApiUrl(network) ,[network])
   return (
-    <ConnectionProvider
-      endpoint={rpcUrl}
-      
-    >
+    <ConnectionProvider endpoint={rpcUrl}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
-          <div className="h-screen w-full flex flex-col justify-center items-center">
-          <Airdrop/>
-        <div className="border border-black">
-          <WalletMultiButton/>
-          <WalletDisconnectButton/>
-        </div>
+          <Image
+            src={bgimg}
+            alt="faucet"
+            className="absolute top-0 left-0 h-screen object-cover"
+          />
+          <div className="h-screen font-custom w-full grid md:grid-cols-2">
+            <div className="h-screen w-full flex flex-col justify-center items-center gap-8 z-10 md:col-span-1">
+              <Airdrop />
+              <div className="border border-black flex flex-col w-max md:min-w-[30rem] px-5 py-2 rounded  items-center  z-10">
+                <Label className="font-bold selection:bg-yellow-700 tracking-wider font-custom text-4xl mt-3 mb-5">
+                  Connect Your Wallet
+                </Label>
+                <ButtonModel/>
+                
+              </div>
+            </div>
+            <div className="hidden md:col-span-1"></div>
           </div>
-        
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
+}
+
+function ButtonModel(){
+  const wallet = useWallet()
+
+  console.log("wallet", wallet.publicKey?.toString())
+
+  return(
+    <div className="flex justify-around items-center w-full">
+                
+                  <WalletMultiButton
+                    style={{
+                      fontSize: "1rem",
+                      width: "100%",
+                      maxWidth: "10rem",
+                      textAlign: "center",
+                      padding: "0.75rem 1rem",
+                    }}
+                  />
+                  {wallet.publicKey && <WalletDisconnectButton
+                    style={{
+                      fontSize: "1rem",
+                      width: "100%",
+                      maxWidth: "10rem",
+                      textAlign: "center",
+                      padding: "0.75rem 1rem",
+                    }}
+                  />}
+                </div>
+
+  )
 }
